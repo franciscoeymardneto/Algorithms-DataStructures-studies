449fcdcca1e1e4221fcba933aa933f91c21ad2c0
eymard <francisco.eymard.oliveira@gmail.com>
2023-04-24 22:42:01 -0300

    male some redux examples

--- DataStructures-Algo-JS-Groner/Arrays/reducer.js
+++ DataStructures-Algo-JS-Groner/Arrays/reducer.js
@@ -1,13 +1,53 @@
 const nums = [1,2,3,4,5,6,7,8]
 
+// O reducer serve para fazer uma redução do array 
+// E para fazer transformações Array => number,string,obj...
+
 // Acc é o valor RETORNADO anteriormente
 // Item é o valor atual
-const sum = nums.reduce((acc,item) => {
-    if (item < acc) {
+const sum = nums.reduce((sum,item) => {
+    if (item < sum) {
         return item
     }
 
-    return acc
+    return sum
 },0)
 
-console.log(sum);
+console.log('SOMA: ', sum);
+
+const jojos = [
+    {
+        name: 'Jonanthan',
+        type: 'Hamon'
+    },
+    {
+        name: 'Joseph',
+        type: 'Hamon'
+    },
+    {
+        name: 'Jotaro',
+        type: 'Stand'
+    },
+    {
+        name: 'Josuke',
+        type: 'Stand'
+    },
+    {
+        name: 'Giorno',
+        type: 'Requien'
+    },
+    {
+        name: 'Jolyne',
+        type: 'Stand'
+    },
+]
+
+const jojosPerType = jojos.reduce((jojosPerType, jojo) => {
+    jojosPerType[jojo.type] = jojosPerType[jojo.type] ?? []
+
+    jojosPerType[jojo.type].push(jojo.name)
+    
+    return jojosPerType
+}, {})
+
+console.log(jojosPerType)
